9469d0f16e8255b0bdbbf80cbbb2f07525033e19
syukurilexs <syukirah@gmail.com>
2024-03-28 16:22:51 +0800

    Update docker build script to include version

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -2,3 +2,9 @@ cd ../
 docker build --pull --rm -f "Dockerfile" -t syukurilexshomesvc:latest "." 
 docker tag syukurilexshomesvc:latest syukurilexs/homesvc:latest
 docker push syukurilexs/homesvc:latest
+
+# Change this version before run
+version=1.1.0
+docker build --pull --rm -f "Dockerfile" -t syukurilexshomesvc:$version "." 
+docker tag syukurilexshomesvc:latest syukurilexs/homesvc:$version
+docker push syukurilexs/homesvc:$version
