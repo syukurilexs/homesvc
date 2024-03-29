8742d081f92745ccc14e59c5c3470e696ab9930b
syukurilexs <syukirah@gmail.com>
2024-03-29 16:54:26 +0800

    include sceneAction to the scene payload

--- src/modules/scene/scene.service.ts
+++ src/modules/scene/scene.service.ts
@@ -108,6 +108,11 @@ export class SceneService {
         sceneDevice: {
           device: true,
         },
+        sceneAction: {
+          action: {
+            device: true
+          }
+        } 
       },
     });
   }
